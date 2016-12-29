8cf589bba6eb9be9f10b56dbb2410735aee2d5fb
Amer Hamdan <amashamdan@gmail.com>
2016-12-29 15:27:53 -0800

    Feat: Added /action route.
    This route handles like and unlike requests.
    The requested action is sent received with the request, database is updated accordibgly.
    Both users and gallerites documents are updated.

--- routes/home.js
+++ routes/home.js
@@ -10,11 +10,48 @@ MongoClient.connect(mongoUrl, function(err, db) {
 		res.end("Error in contacting database");
 	} else {
 		var gallerites = db.collection("gallerites");
+		var users = db.collection("users");
+
 		router.route("/")
 		.get(function(req, res) {
 			gallerites.find({}).toArray(function(err, results) {
 				res.render("index.ejs", {user: req.user, gallerites: results});
 			});
+		});
+
+		router.route("/action/:serialNumber")
+		.post(function(req, res) {
+			if (req.body.action == "like") {
+				gallerites.update(
+					{"serialNumber": Number(req.params.serialNumber)},
+					{"$addToSet": {"likedBy": req.user.id}},
+					function() {
+						users.update(
+							{"userId": req.user.id},
+							{"$addToSet": {"likedGallerites": Number(req.params.serialNumber)}},
+							function() {
+								res.status(200);
+								res.end();
+							}
+						);
+					}
+				);
+			} else if (req.body.action == "unlike") {
+				gallerites.update(
+					{"serialNumber": Number(req.params.serialNumber)},
+					{"$pull": {"likedBy": req.user.id}},
+					function() {
+						users.update(
+							{"userId": req.user.id},
+							{"$pull": {"likedGallerites": Number(req.params.serialNumber)}},
+							function() {
+								res.status(200);
+								res.end();
+							}
+						);
+					}
+				);				
+			}
 		});
 	}
 });
