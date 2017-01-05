2a232702b0b03b0da82a12121a419719e85eda6e
Amer Hamdan <amashamdan@gmail.com>
2017-01-05 12:51:32 -0800

    Feat and Refactor: Added callback to get request to /search/:tag and refactored post request callback.
    get request callback is added and it is very similar to that of the post request.
    The identical parts are moved to a seperate function getData which is called from both callback functions.

--- routes/search.js
+++ routes/search.js
@@ -7,7 +7,40 @@ var router = express.Router();
 var MongoClient = mongodb.MongoClient;
 var mongoUrl = process.env.GALLERIA;
 
+MongoClient.connect(mongoUrl, function(err, db) {
+	router.route("/")
+	.post(parser, function(req, res) {
+		var entry = req.body.input;
+		getData(req, res, db, entry);
+	})
 
+	router.route("/:entry")
+	.get(function(req, res) {
+		var entry = req.params.entry;
+		getData(req, res, db, entry)
+	})
+});
+
+function getData(req, res, db, entry) {
+	if (!req.user) {
+		var user = undefined;
+	} else {
+		var user = req.user;
+	}
+	var gallerites = db.collection("gallerites");
+	var formattedEntry = entry.toLowerCase();
+	formattedEntry = entry.replace(" ", "-");
+
+	gallerites.find({"tags": formattedEntry}).toArray(function(err, results) {
+		res.render("search.ejs", {user: user, gallerites: results, entry: entry});
+	});
+}
+
+module.exports = router;
+
+
+
+/*
 router.route("/")
 .post(parser, function(req, res) {
 	MongoClient.connect(mongoUrl, function(err, db) {
@@ -32,6 +65,7 @@ router.route("/")
 			res.render("search.ejs", {user: user, gallerites: results, entry: req.body.input});
 		});
 	})
-});
+})
+.get(function(req, res) {
 
-module.exports = router;
+})*/
