fd631bd4feae5cb363f1580be4e18c5b42171aab
Amer Hamdan <amashamdan@gmail.com>
2017-01-05 14:12:40 -0800

    Refactor: Removed commented out lines.

--- routes/search.js
+++ routes/search.js
@@ -57,34 +57,3 @@ function getData(req, res, db, entry) {
 
 module.exports = router;
 
-
-
-/*
-router.route("/")
-.post(parser, function(req, res) {
-	MongoClient.connect(mongoUrl, function(err, db) {
-		if (err) {
-			res.end("Error connecting to database");
-			return
-		}
-
-		if (!req.user) {
-			var user = undefined;
-		} else {
-			var user = req.user;
-		}
-
-		var gallerites = db.collection("gallerites");
-
-		var entry = req.body.input;
-		entry = entry.toLowerCase();
-		entry = entry.replace(" ", "-");
-
-		gallerites.find({"tags": entry}).toArray(function(err, results) {
-			res.render("search.ejs", {user: user, gallerites: results, entry: req.body.input});
-		});
-	})
-})
-.get(function(req, res) {
-
-})*/
